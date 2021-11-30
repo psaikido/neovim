ea20cb698bea0f761637543c6ba41a672ea0cd88
Hugh Carroll <hughie@carrollonline.co.uk>
2021-11-30 19:44:00 +0000

    A few tweaks to preferences and a way in for two vimwikis to be got at by usb on a new system.

--- lua/plugin-config.lua
+++ lua/plugin-config.lua
@@ -14,24 +14,30 @@ local keymap = function(key)
     vim.api.nvim_set_keymap (key[1], key[2], key[3], opts)
 end
 
+
 -- General Usage
 keymap {'n', '<C-s>', ':w<CR>'}
 keymap {'n', '<C-c>', ':q<CR>'}
 keymap {'n', '<Esc>', ':nohlsearch<CR>'}
 keymap {'i', 'kj', '<Esc>'}
 
+
 -- Consider files of 10Mg and above as 'large'
 vim.g.LargeFile = 1
+
 
 -- Airline
 vim.g.airline_theme = 'solarized'
 vim.g.airline_solarized_bg = 'dark'
 
+
 -- fugitive - invoke git status
 keymap {'n', '<Leader>g', ':Git<CR>'}
 
+
 -- Lifetrak
 vim.g.lifetrak_metas = {'energy', 'pain', 'mood', 'sleep'}
+
 
 -- Markdown preview
 -- run after install if it doesn't happen automatically - :call mkdp#util#install()'})
@@ -39,9 +45,11 @@ keymap {'n', '<Leader>mkp', ':MarkdownPreview<CR>'}
 keymap {'n', '<Leader>mks', ':MarkdownPreviewStop<CR>'}
 keymap {'n', '<Leader>mkt', ':MarkdownPreviewToggle<CR>'}
 
+
 -- NeoSolarized
 vim.cmd('colorscheme NeoSolarized')
 vim.cmd('set background=dark')
+
 
 -- NERDTree
 keymap {'n', '<leader>e', ':NERDTree<CR>'}
@@ -50,8 +58,10 @@ keymap {'n', '<leader>b', ':Bookmark<CR>'}
 vim.cmd('let NERDTreeShowBookmarks = 1')
 vim.cmd('let NERDTreeMinimalUI = 1')
 
+
 -- nvim-colorizer
 require'colorizer'.setup()
+
 
 -- telescope
 keymap { 'n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>' }
@@ -60,14 +70,30 @@ keymap { 'n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>
 keymap { 'n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>' }
 keymap { 'n', '<leader>f~', '<cmd>lua require("telescope.builtin").file_browser({cwd="/home/hughie/"})<cr>' }
 
+
 -- terminal
 keymap {'n', '<leader>t', ':sp <Bar> te<CR><C-w><C-r>'}
 keymap {'t', '<Esc>', '<C-\\><C-n>'}
 
+
 -- vimwiki
-vim.cmd("let g:vimwiki_list = [{'path': '~/crypt/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]")
+vim.cmd("let wiki_tech = {}")
+vim.cmd("let wiki_tech.path = '~/crypt/vimwiki/tech/'")
+vim.cmd("let wiki_tech.syntax = 'markdown'")
+vim.cmd("let wiki_tech.ext = 'md'")
+
+vim.cmd("let wiki_hc = {}")
+vim.cmd("let wiki_hc.path = '~/crypt/vimwiki/hc/'")
+vim.cmd("let wiki_hc.syntax = 'markdown'")
+vim.cmd("let wiki_hc.ext = 'md'")
+
+vim.cmd(
+    "let g:vimwiki_list = [wiki_tech, wiki_hc]"
+)
+
 keymap {'n', '<leader>w<leader>h', ':VimwikiSplitLink<cr>'}
 keymap {'n', '<leader>w<leader>v', ':VimwikiVSplitLink<cr>'}
+
 
 -- which-key
 require("which-key").setup {}

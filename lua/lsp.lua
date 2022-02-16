e83af4881a0c3d01bfb769498ac9438bf24eab33
Hugh Carroll <hughie@carrollonline.co.uk>
2022-02-16 22:40:38 +0000

    Add css lsp language server.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -32,9 +32,16 @@ local on_attach = function(client, bufnr)
 
 end
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'phpactor' }
+local servers = { 'pyright', 'phpactor', 'cssls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,

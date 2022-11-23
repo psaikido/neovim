66f957517b7845b12f5fb2e1adfdab0d296a4805
Hugh Carroll <hughie@carrollonline.co.uk>
2022-11-23 09:42:26 +0000

    Open harpoon files in vsp or new tab.

--- lua/options.lua
+++ lua/options.lua
@@ -75,3 +75,26 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown",
     command = "setlocal nospell"
 })
+
+-- Harpoon - open in split
+local group = vim.api.nvim_create_augroup("Harpoon Augroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "harpoon",
+    group = group,
+    callback = function()
+        vim.keymap.set("n", "<C-V>", function()
+            local curline = vim.api.nvim_get_current_line()
+            local working_directory = vim.fn.getcwd() .. "/"
+            vim.cmd("vs")
+            vim.cmd("e " .. working_directory .. curline)
+        end, { buffer=true, noremap = true, silent = true })
+    end,
+    callback = function()
+        vim.keymap.set("n", "<C-t>", function()
+            local curline = vim.api.nvim_get_current_line()
+            local working_directory = vim.fn.getcwd() .. "/"
+            vim.cmd("tabnew")
+            vim.cmd("e " .. working_directory .. curline)
+        end, { buffer=true, noremap = true, silent = true })
+    end,
+})

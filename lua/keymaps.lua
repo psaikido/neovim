8ab48e3e485d2bab1f3bd3624456fc93b9bb323f
Hugh Carroll <hughie@carrollonline.co.uk>
2022-12-04 14:51:00 +0000

    Add tmux nav keymaps so they are reapplied when I blat and restore my config.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -99,3 +99,8 @@ P = function(v)
     return v
 end
 
+-- tmux navigation
+ vim.keymap.set('n', '<C-h>', ':<C-U>TmuxNavigateLeft<cr>', {})
+ vim.keymap.set('n', '<C-l>', ':<C-U>TmuxNavigateRight<cr>', {})
+ vim.keymap.set('n', '<C-j>', ':<C-U>TmuxNavigateDown<cr>', {})
+ vim.keymap.set('n', '<C-k>', ':<C-U>TmuxNavigateUp<cr>', {})

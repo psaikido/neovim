ca5462b113713ad778437418fdaebded418bd5a0
Hugh Carroll <hughie@carrollonline.co.uk>
2022-12-05 18:52:52 +0000

    Bit closer to hupoon.

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,6 +3,9 @@ local uetel = {}
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
 local conf = require("telescope.config").values
+local themes = require('telescope.themes')
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 
 
 require('telescope').setup {
@@ -53,7 +56,8 @@ end
 uetel.hupoon = function()
     opts = opts or {}
     pickers.new(opts, {
-        prompt_title = "colors",
+        prompt_title = "hupoon",
+
         finder = finders.new_table {
             results = {
                 '~/.config',
@@ -67,8 +71,23 @@ uetel.hupoon = function()
                 '~/sites/chanadmin',
             }
         },
+
         sorter = conf.generic_sorter(opts),
-    }):find()
+
+        layout_config = {
+            width = 0.5 
+        }
+
+        -- attach_mappings = function(prompt_bufnr, map)
+        --     actions.select_default:replace(function()
+        --         actions.close(prompt_bufnr)
+        --         local selection = action_state.get_selected_entry()
+        --         -- print(vim.inspect(selection))
+        --         vim.api.nvim_put({ selection[1] }, "", false, true)
+        --     end)
+        --     return true
+        -- end,
+    }):find(themes.get_dropdown())
 end
 
 require('telescope').load_extension('media_files')

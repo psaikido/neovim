d86f9f9df48241c41ba0f783917df946d1ad20b9
Hugh Carroll <hughie@carrollonline.co.uk>
2022-12-12 15:48:56 +0000

    Start setting up an initialisation.

--- lua/config/lifetrak.lua
+++ lua/config/lifetrak.lua
@@ -3,7 +3,8 @@ local M = {}
 function M.setup()
     require('lifetrak').setup(
         {
-            journal = '~/journal.lft',
+            journal= '~/journal.lft',
+            -- journal= '~/crypt/journal/journal.lft',
         }
     )
 end

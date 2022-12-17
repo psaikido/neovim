56484fc6fb2052a888d3e2e6facb9db0a59ec8db
Hugh Carroll <hughie@carrollonline.co.uk>
2022-12-17 01:49:02 +0000

    Add the language servers I'm going to use.

--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ require('config.telescope')
 
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.ensure_installed({
+    'pyright',
+    'phpactor',
+    'sumneko_lua',
+})
+lsp.nvim_workspace()
 lsp.setup()
 
 require('config/lifetrak').setup()

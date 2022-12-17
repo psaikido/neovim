42f4c48e9a8e1f5fef79a61cb6ef93fcc0b9f0ff
Hugh Carroll <hughie@carrollonline.co.uk>
2022-12-17 09:42:37 +0000

    Reshuffle my setup. Influenced by https://github.com/ThePrimeagen/init.lua

--- init.lua
+++ init.lua
@@ -1,17 +1,6 @@
 require('plugins').setup()
 require('utils')
-require('config.telescope')
-
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-lsp.ensure_installed({
-    'pyright',
-    'phpactor',
-    'sumneko_lua',
-})
-lsp.nvim_workspace()
-lsp.setup()
-
+require('config/telescope')
+require('config/lsp').setup()
 require('config/lifetrak').setup()
 require('config/hupoon').setup()
-

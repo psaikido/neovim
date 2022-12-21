4215bbe715f4f574cc2d856c3f5016be03e9261a
Hugh Carroll <hughie@carrollonline.co.uk>
2022-12-21 23:23:15 +0000

    Add filter by tag and metas to the user commands for lifetrak setup.

--- lua/hc/lifetrak.lua
+++ lua/hc/lifetrak.lua
@@ -18,12 +18,9 @@ function M.setup()
     require('lifetrak').init(config)
 end
 
--- command! LifetrakFilterMetas execute ":lua require('lifetrak.filter_metas').choose_meta()"
--- command! LifetrakFilterTags execute ":lua require('lifetrak').choose_tag()"
 local lifetrak = require('lifetrak')
--- local filter_tags = require('lifetrak.filter_tags')
--- local filter_metas = require('lifetrak.filter_metas')
--- local lifetrak_utils = require('lifetrak.utils')
+local filter_tags = require('lifetrak.filter_tags')
+local filter_metas = require('lifetrak.filter_metas')
 
 vim.api.nvim_create_user_command('Lifetrak', function()
     lifetrak.open_current()
@@ -41,6 +38,18 @@ vim.api.nvim_create_user_command('LifetrakEntry', function()
     lifetrak.journal_entry()
   end,
   {nargs = 0, desc = 'New journal entry'}
+)
+
+vim.api.nvim_create_user_command('LifetrakFilterTags', function()
+    filter_tags.choose_tag()
+  end,
+  {nargs = 0, desc = 'Filter by tag'}
+)
+
+vim.api.nvim_create_user_command('LifetrakFilterMetas', function()
+    filter_metas.choose_meta()
+  end,
+  {nargs = 0, desc = 'Filter by tag'}
 )
 
 return M

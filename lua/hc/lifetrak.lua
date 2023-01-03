603f7de885fe49e30aa5f472799c1b4dcaa07922
Hugh Carroll <hughie@carrollonline.co.uk>
2023-01-03 12:40:14 +0000

    Move the user commands to the plugin.

--- lua/hc/lifetrak.lua
+++ lua/hc/lifetrak.lua
@@ -22,35 +22,5 @@ local lifetrak = require('lifetrak')
 local filter_tags = require('lifetrak.filter_tags')
 local filter_metas = require('lifetrak.filter_metas')
 
-vim.api.nvim_create_user_command('Lifetrak', function()
-    lifetrak.open_current()
-  end,
-  {nargs = 0, desc = 'Open current journal'}
-)
-
-vim.api.nvim_create_user_command('LifetrakChangeCurrent', function()
-    lifetrak.change_current()
-  end,
-  {nargs = 0, desc = 'Change current journal'}
-)
-
-vim.api.nvim_create_user_command('LifetrakEntry', function()
-    lifetrak.journal_entry()
-  end,
-  {nargs = 0, desc = 'New journal entry'}
-)
-
-vim.api.nvim_create_user_command('LifetrakFilterTags', function()
-    filter_tags.choose_tag()
-  end,
-  {nargs = 0, desc = 'Filter by tag'}
-)
-
-vim.api.nvim_create_user_command('LifetrakFilterMetas', function()
-    filter_metas.choose_meta()
-  end,
-  {nargs = 0, desc = 'Filter by tag'}
-)
-
 return M
 

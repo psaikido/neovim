d76ef27cb61d02c96b4e9cc2e1bd6535b2cbc448
Hugh Carroll <hughie@carrollonline.co.uk>
2023-10-14 10:10:13 +0100

    Format the emmet doctype/html5 template.

--- lua/hc/emmet.lua
+++ lua/hc/emmet.lua
@@ -1,19 +1,20 @@
 vim.g.user_emmet_settings = {
 	html = {
 		snippets = {
-			html5 = [[<!DOCTYPE html>
-							<html lang="en">
-							<head>
-								<meta charset="UTF-8">
-								<meta name="viewport" content="width=device-width, initial-scale=1.0">
-								<meta name="author" value="Hugh Carroll https://hugle.uk">
-								<title></title>
-								<link rel="stylesheet" href="">
-							</head>
-							<body>
-								
-							</body>
-							</html>]]
+			html5 = [[
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" value="Hugh Carroll - hughie [at] hugle.uk">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>]]
 		}
 	}
 }
